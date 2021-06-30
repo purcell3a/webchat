e8832f236463979ec694b01dd73c58f6c1bb2704
Angela Purcell <angelapurcell@Angelas-MBP.attlocal.net>
2021-06-30 14:37:52 -0700

    webserver running

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WEBCHAT
+
+### Mapping out the structure
+- responses should be stored as type convo map[string]string?
--- main.go
+++ main.go
@@ -2,8 +2,49 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
+const form = `<html><body><form action="#" method="post" name="bar">
+<input type="text" name="in"/>
+<input type="submit" value="Submit"/>
+</form></html></body>`
+
+/* handle a simple get request */
+func SimpleServer(w http.ResponseWriter, request *http.Request) {
+	io.WriteString(w, "<h1>hello, world</h1>")
+}
+
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Inside HelloServer handler")
+	fmt.Fprint(w, "Hello, "+req.URL.Path[1:])
+}
+
+/* handle a form, both the GET which displays the form
+and the POST which processes it.*/
+func FormServer(w http.ResponseWriter, request *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	switch request.Method {
+	case "GET":
+		/* display the form to the user */
+		io.WriteString(w, form)
+	case "POST":
+		/* handle the form data, note that ParseForm must
+		be called before we can extract form data with Form */
+		// request.ParseForm();
+		//io.WriteString(w, request.Form["in"][0])
+		// easier method:
+		io.WriteString(w, request.FormValue("in"))
+	}
+}
+
 func main() {
-	fmt.Println("testing")
+
+	http.HandleFunc("/test1", SimpleServer)
+	http.HandleFunc("/test2", FormServer)
+	if err := http.ListenAndServe("0.0.0.0:3000", nil); err != nil {
+		panic(err)
+	}
 }
